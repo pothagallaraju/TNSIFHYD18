5c2226e9f74d81a87265cf3372e39518a83e6850
pothagallaraju <168468399+pothagallaraju@users.noreply.github.com>
2024-05-28 12:50:12 +0530

    Add files via upload

---
+++ Approach.java
@@ -0,0 +1,10 @@
+package first_java_project;
+
+public class Approach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
