06d8b160d8e963775a3f7f25a58d1e325fe87463
pothagallaraju <168468399+pothagallaraju@users.noreply.github.com>
2024-05-28 16:02:20 +0530

    Add files via upload

---
+++ SpringBootDI/src/main/java/com/example/demo/IPL.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IPL {
+	
+	public void Updates() {
+		System.out.println("2024 IPL Teams and Schdule is updating!");
+
+	}
+
+}
